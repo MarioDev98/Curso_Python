1f92da5c7481d2375b6cae83d6f94f4a6ef64801
Mario Medina <mariomedinalucero29@gmail.com>
2025-02-03 11:19:03 -0600

    operaciones basicas y ejercicio

---
+++ Ejercicios_operaciones_basicas.py
@@ -0,0 +1,18 @@
+# Obtener el promedio de 5 calificaciones del alumno que se llama Juan
+# Calificaciones de Juan = 5 8 9 10 3
+# Promedio = 7
+
+#Datos de entrada
+calif1 = 5
+calif2 = 8
+calif3 = 9
+calif4 = 10 
+calif5 = 3
+
+#Flujo de control
+resultado_suma_calificaciones = calif1 + calif2 + calif3 + calif4 + calif5 
+#resultado_promedio = (calif1 + calif2 + calif3 + calif4 + calif5) / 5
+resultado_promedio = resultado_suma_calificaciones / 5
+
+# Salida resultados
+print("El promedio de Juan  es = ", resultado_promedio)
---
+++ operaciones_basicas.py
@@ -0,0 +1,19 @@
+#Datos de entrada
+
+#Enteros
+numero1 = 10
+numero2 = 2
+
+#Decimales
+# num1 = 10.2
+# num2 = 2
+
+# Flujo de control
+# resultado_suma = numero1 + numero2
+# resultado_resta = numero1 - numero2
+# resultado_multiplicacion = numero1 * numero2
+resultado_division = numero1 / numero2
+
+#Salida
+print("El resultado de la división es = ", resultado_division )
+
