7d5446a780ce1fb3405444923b62748f895a1378
Mario Medina <mariomedinalucero29@gmail.com>
2025-02-03 01:40:34 -0600

    hola mundo y tipo de datos

---
+++ mi_primer_programa.py
@@ -0,0 +1,10 @@
+
+#Hola mundo en un print
+print("Hola mundo")
+
+# Imprimir texto dentro de un print con una variable
+# texto= "Hola mundo en variable"
+# print(texto)
+
+
+
---
+++ tipos_datos_python.py
@@ -0,0 +1,17 @@
+# Tipo de dato entero
+edad = 55
+print("Tipo de dato entero edad = ", edad)
+
+# Tipo de dato float
+promedio = 8.3
+print("Tipo de dato float - el promedio es = ",promedio)
+
+# #Tipo de dato cadenas de texto
+texto = "Mario Medina"
+print(" Mi nombre es = ",texto)
+
+# #Tipo de datos boolenos (verdadero o falso)
+verdadero = True
+falso= False
+print("Esto es verdadero =",verdadero)
+print("Esto es falso =",falso)
